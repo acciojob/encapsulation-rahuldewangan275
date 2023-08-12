8f39948c4d4aa46417fbab902cad6652458ed406
Rahul Kumar Dewangan <rahuldewangan275@gmail.com>
2023-08-12 22:36:23 +0530

    First Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main (String[] args){
+
+      RWOnly obj = new RWOnly();
+//      obj.name ="Rahul"; // ERROR : java: name has private access in com.driver.RWOnly
+//      System.out.println();
+       obj.setter("Rahul");
+       System.out.println(obj.getter());
+  }
 }
